7bce7a25fc13e576683a9b47b79eb25ae1ce0c3b
Stephen Sherratt <stephen@sherra.tt>
2021-08-08 14:56:54 +1000

    Add wgpu-wip crate

---
+++ wgpu-wip/src/main.rs
@@ -0,0 +1,32 @@
+use chargrid_wgpu::*;
+use orbital_decay_app_wip::app;
+
+const CELL_SIZE: f64 = 12.;
+
+fn main() {
+    env_logger::init();
+    let context = Context::new(Config {
+        font_bytes: FontBytes {
+            normal: include_bytes!("./fonts/PxPlus_IBM_CGAthin-with-quadrant-blocks.ttf").to_vec(),
+            bold: include_bytes!("./fonts/PxPlus_IBM_CGA-with-quadrant-blocks.ttf").to_vec(),
+        },
+        title: "Orbital Decay".to_string(),
+        window_dimensions_px: Dimensions {
+            width: 960.,
+            height: 720.,
+        },
+        cell_dimensions_px: Dimensions {
+            width: CELL_SIZE,
+            height: CELL_SIZE,
+        },
+        font_scale: Dimensions {
+            width: CELL_SIZE,
+            height: CELL_SIZE,
+        },
+        underline_width_cell_ratio: 0.1,
+        underline_top_offset_cell_ratio: 0.8,
+        resizable: false,
+    });
+
+    context.run(app());
+}
