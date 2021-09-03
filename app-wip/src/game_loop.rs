05d8f27bf942b7f4fa8e035aa03621e63bdd0921
Stephen Sherratt <stephen@sherra.tt>
2021-09-03 22:35:26 +1000

    Begin work on pause menu

--- app-wip/src/game_loop.rs
+++ app-wip/src/game_loop.rs
@@ -24,6 +24,38 @@ impl GameLoopState {
             .render_with_visibility(self.game.inner_ref().visibility_grid(), ctx, fb);
         game::render_game(self.game.inner_ref(), ctx, fb);
     }
+    fn has_game_ever_been_won(&self) -> bool {
+        // TODO
+        true
+    }
+    fn update(&mut self, event: Event, running: witness::Running) -> Witness {
+        match event {
+            Event::Input(Input::Keyboard(keyboard_input)) => {
+                if let Some(app_input) = self.controls.get(keyboard_input) {
+                    let (witness, action_result) = match app_input {
+                        AppInput::Move(direction) => {
+                            running.walk(&mut self.game, direction, &self.config)
+                        }
+                        AppInput::Wait => running.wait(&mut self.game, &self.config),
+                        AppInput::Examine | AppInput::Aim(_) | AppInput::Get => {
+                            println!("todo");
+                            (Witness::Running(running), Ok(()))
+                        }
+                    };
+                    if let Err(action_error) = action_result {
+                        println!("action error: {:?}", action_error);
+                    }
+                    witness
+                } else {
+                    Witness::Running(running)
+                }
+            }
+            Event::Tick(since_previous) => {
+                running.tick(&mut self.game, since_previous, &self.config)
+            }
+            _ => Witness::Running(running),
+        }
+    }
 }
 
 impl GameLoopState {
@@ -45,6 +77,11 @@ impl GameLoopState {
 
 struct GameInstanceComponent(Option<witness::Running>);
 
+enum AppState {
+    Paused(witness::Running),
+    Playing(Witness),
+}
+
 impl Component for GameInstanceComponent {
     type Output = Witness;
     type State = GameLoopState;
@@ -54,31 +91,10 @@ impl Component for GameInstanceComponent {
 
     fn update(&mut self, state: &mut Self::State, _ctx: Ctx, event: Event) -> Self::Output {
         let running = self.0.take().unwrap();
-        match event {
-            Event::Input(Input::Keyboard(keyboard_input)) => {
-                if let Some(app_input) = state.controls.get(keyboard_input) {
-                    let (witness, action_result) = match app_input {
-                        AppInput::Move(direction) => {
-                            running.walk(&mut state.game, direction, &state.config)
-                        }
-                        AppInput::Wait => running.wait(&mut state.game, &state.config),
-                        AppInput::Examine | AppInput::Aim(_) | AppInput::Get => {
-                            println!("todo");
-                            (Witness::Running(running), Ok(()))
-                        }
-                    };
-                    if let Err(action_error) = action_result {
-                        println!("action error: {:?}", action_error);
-                    }
-                    witness
-                } else {
-                    Witness::Running(running)
-                }
-            }
-            Event::Tick(since_previous) => {
-                running.tick(&mut state.game, since_previous, &state.config)
-            }
-            _ => Witness::Running(running),
+        if event.is_escape() {
+            Witness::Running(running)
+        } else {
+            state.update(event, running)
         }
     }
 
@@ -143,10 +159,61 @@ fn upgrade_component(
     })
 }
 
+#[derive(Clone)]
+enum PauseMenuEntry {
+    Resume,
+    SaveQuit,
+    NewGame,
+    Options,
+    Help,
+    Prologue,
+    Epilogue,
+    Clear,
+}
+
+fn pause_menu() -> CF<impl Component<State = GameLoopState, Output = Option<PauseMenuEntry>>> {
+    on_state_then(|state: &mut GameLoopState| {
+        use menu::builder::*;
+        use PauseMenuEntry::*;
+        let mut builder = menu_builder();
+        let mut add_item = |entry, name, ch: char| {
+            builder.add_item_mut(
+                item(Resume, identifier::simple(&format!("({}) {}", ch, name))).add_hotkey_char(ch),
+            );
+        };
+        add_item(Resume, "Resume", 'r');
+        add_item(SaveQuit, "Save and Quit", 'q');
+        add_item(NewGame, "New Game", 'n');
+        add_item(Options, "Options", 'o');
+        add_item(Help, "Help", 'h');
+        add_item(Prologue, "Prologue", 'p');
+        if state.has_game_ever_been_won() {
+            add_item(Epilogue, "Epilogue", 'e');
+        }
+        add_item(Clear, "Clear", 'c');
+        builder
+            .build_cf()
+            .border(BorderStyle::default())
+            .centre()
+            .overlay(
+                render_state(|state: &GameLoopState, ctx, fb| state.render(ctx, fb)),
+                chargrid::core::TintDim(63),
+                10,
+            )
+    })
+}
+
 fn game_instance_component(
     running: witness::Running,
 ) -> CF<impl Component<State = GameLoopState, Output = Option<Witness>>> {
-    cf(GameInstanceComponent(Some(running))).some()
+    either!(Ei = A | B);
+    cf(GameInstanceComponent(Some(running)))
+        .some()
+        .catch_escape()
+        .and_then(|or_escape| match or_escape {
+            Ok(witness) => Ei::A(val_once(witness)),
+            Err(Escape) => Ei::B(never()),
+        })
 }
 
 pub enum GameExitReason {
