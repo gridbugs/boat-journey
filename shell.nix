33ea134d2d006d3dfcd769e3a454193e98eb013b
Stephen Sherratt <stephen@sherra.tt>
2023-03-05 13:20:24 +1100

    Remove flakes, update deps

--- shell.nix
+++ shell.nix
@@ -1,9 +1,44 @@
-(import (
-  let
-    lock = builtins.fromJSON (builtins.readFile ./flake.lock);
-  in fetchTarball {
-    url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
-    sha256 = lock.nodes.flake-compat.locked.narHash; }
-) {
-  src =  ./.;
-}).shellNix
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell rec {
+  packages = with pkgs; [
+    rustc
+    cargo
+    rustPlatform.rustLibSrc
+    rust-analyzer
+    cargo-watch
+    rustfmt
+    pkg-config
+    cmake
+
+    # Graphics and Audio Dependencies
+    alsa-lib
+    libao
+    openal
+    libpulseaudio
+    udev
+    fontconfig
+    xorg.libX11
+    xorg.libXcursor
+    xorg.libXrandr
+    xorg.libXi
+    vulkan-loader
+    vulkan-tools
+    libGL
+    bzip2
+    zlib
+    libpng
+    expat
+    brotli
+
+    # JS/Wasm Deps
+    nodejs
+    wasm-pack
+  ];
+
+  # Allows rust-analyzer to find the rust source
+  RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+
+  # Without this graphical frontends can't find the GPU adapters
+  LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath packages}";
+}
